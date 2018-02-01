8c48c9cedca55212d5e4ca1ae8d6def1e128c31d
IAMSUTHICHA <suthicha@outlook.com>
2018-02-01 18:26:50 +0700

    add autobot service

---
+++ TMSBookingRobot.Controllers/BookingAutoBot.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+
+namespace TMSBookingRobot.Controllers
+{
+    public class BookingAutoBot
+    {
+        private string _sqlConnection;
+        private string _destFolder;
+
+        public BookingAutoBot(string sqlConnectionString, string destFolder)
+        {
+            _sqlConnection = sqlConnectionString;
+            _destFolder = destFolder;
+        }
+
+        public void Live()
+        {
+            var controller = new BookingController();
+            controller.SetConnection(_sqlConnection);
+
+            var queue = controller.GetBookingQueueFromSysF();
+
+            if (queue == null || queue.Count == 0) return;
+
+            for (int i = 0; i < queue.Count; i++)
+            {
+                var bookingQueueItem = queue[i];
+                Logger.EventLog("[BookingController.cs][RunAutoBot] : Create job " + bookingQueueItem.BookingNo);
+
+                var bookingJob = controller.GetBooking(bookingQueueItem.BookingNo);
+
+                if (bookingJob == null) continue;
+                Logger.EventLog("[BookingController.cs][RunAutoBot] : Find job " + bookingQueueItem.BookingNo);
+
+                var destFileName = Path.Combine(_destFolder, bookingJob + ".xlsx");
+
+                if (controller.ExportToExcel(bookingJob, destFileName))
+                {
+                    controller.FlagBookingQueueExportCompleted(bookingJob.BookingNo);
+                    Logger.EventLog("[BookingController.cs][RunAutoBot] : Write excel " + destFileName);
+                }
+            }
+        }
+    }
+}
---
+++ TMSBookingRobot.WinService/Service1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using TMSBookingRobot.Controllers;
+
+namespace TMSBookingRobot.WinService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer1.Interval = 3000;
+            try
+            {
+                timer1.Interval = Convert.ToInt32(ConfigurationManager.AppSettings["interval"]);
+                timer1.Start();
+                Logger.EventLog("Start Service");
+            }
+            catch { }
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Stop();
+            Logger.EventLog("Stop Service");
+        }
+
+        private void timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            timer1.Enabled = false;
+            try
+            {
+                var sqlConnectionString = ConfigurationManager.AppSettings["connection"];
+                var destFolder = ConfigurationManager.AppSettings["destFolder"];
+
+                var autobot = new BookingAutoBot(sqlConnectionString, destFolder);
+                autobot.Live();
+            }
+            catch (Exception ex)
+            {
+                Logger.ErrorLog("[Service1.cs][timer1_Elapsed]" + ex.Message);
+            }
+            timer1.Enabled = true;
+        }
+    }
+}
