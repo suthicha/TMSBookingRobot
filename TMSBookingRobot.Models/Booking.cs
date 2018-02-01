25bf7cde8f092e72bd90a0d3fa6a3008237014f5
Suthicha Poonakaow <suthicha@outlook.com>
2018-02-01 10:52:38 +0700

    add Booking model

---
+++ TMSBookingRobot.Models/Booking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TMSBookingRobot.Models
+{
+    public class Booking
+    {
+        public string TrxNo {get; set;}
+        public string BookingNo {get; set;}
+        public string ShipperCode {get;set;}
+    }
+}
